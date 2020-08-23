9cae4efe2956005c657dcedc36dd5498e47c1f8d
jacksmith199742 <jacksmith199742@gmail.com>
2020-08-23 18:26:43 +0900

    appbar customing2

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -3,6 +3,13 @@ import { withStyles, AppBar, Grid, Button } from '@material-ui/core';
 import PropTypes from "prop-types";
 import { useLocation } from 'react-router-dom';
 
+const pageRoutes = [
+  { path: "/home", label: "Comentar empresa" },
+  { path: "/home/c", label: "Qué es" },
+  { path: "/home/a", label: "Empresas" },
+  { path: "/home/b", label: "Contacto" },
+];
+
 const headerStyle = {
   root: {
     background: "#FFFFFF 0% 0% no-repeat padding-box",
@@ -31,11 +38,30 @@ const headerStyle = {
     letterSpacing: -0.15,
     color: "#717171",
   },
+  activeLink: {
+    padding: "0px 20px",
+    font: "normal normal medium 15px/17px Helvetica Neue",
+    letterSpacing: -0.15,
+    color: "#202124",
+    borderBottom: "2px solid #8A56AC",
+  },
+  accessLink: {
+    padding: "0px 20px",
+    font: "normal normal normal 18px/22px Helvetica Neue",
+    letterSpacing: -0.18,
+    color: "#8A56AC",
+  },
+  companyLink: {
+    background: "#8A56AC 0% 0% no-repeat padding-box",
+    borderRadius: 2,
+    padding: "12px 10px",
+    font: "normal normal normal 18px/22px Helvetica Neue",
+    letterSpacing: -0.18,
+    color: "#FFFFFF",
+  },
 };
 
-const pageRoutes = [
 
-];
 
 const MainHeader = (props: any) => {
 
@@ -46,6 +72,8 @@ const MainHeader = (props: any) => {
   console.log(location);
 
   const activeRoute = (routeName: string) => {
+    console.log(location.pathname);
+    console.log(routeName);
     return location.pathname.indexOf(routeName) > -1 ? true : false;
   }
 
@@ -59,12 +87,26 @@ const MainHeader = (props: any) => {
         </Grid>
         <Grid item className={classes.rest}>
           <Grid container justify="space-between" alignItems="stretch" className={classes.fullHeight}>
-            <Button className={classes.navLink}>Comentar empresa</Button>
+            {
+              pageRoutes.map((prop, key) => {
+                if (activeRoute(prop.path)) {
+                  return <Button className={classes.activeLink} key={key}>{prop.label}</Button>
+                }
+                return <Button className={classes.navLink} key={key}>{prop.label}</Button>
+              })
+            }
+            <Button className={classes.accessLink}>Acceder</Button>
+            <Grid item>
+              <Grid container alignItems="center" className={classes.fullHeight}>
+                <Button className={classes.companyLink}>Acceder como empresa</Button>
+              </Grid>
+            </Grid>
+            {/* <Button className={classes.navLink}>Comentar empresa</Button>
             <Button className={classes.navLink}>Qué es</Button>
             <Button className={classes.navLink}>Empresas</Button>
             <Button className={classes.navLink}>Contacto</Button>
             <Button className={classes.navLink}>Acceder</Button>
-            <Button className={classes.navLink}>Acceder como empresa</Button>
+            <Button className={classes.navLink}>Acceder como empresa</Button> */}
             {/* <Grid item>Comentar empresa</Grid>
             <Grid item>Qué es</Grid>
             <Grid item>Empresas</Grid>
