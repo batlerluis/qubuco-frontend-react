bb79b4d37425604f8c5ae5aa72c51e05f7b15fb2
james424smith@gmail.com <james424smith@gmail.com>
2020-08-25 00:54:24 +0900

    survey_ratingPage

--- src/views/SruveyStart.tsx
+++ src/views/SruveyStart.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -10,23 +10,44 @@ import Button from '@material-ui/core/Button';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import SettingsBackupRestoreIcon from '@material-ui/icons/SettingsBackupRestore';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Box from '@material-ui/core/Box';
+import Rating from '@material-ui/lab/Rating';
+
+
 
 import imgLog from "../assets/img/car.png"
+import { createMuiTheme } from '@material-ui/core/styles';
+import deepPurple from '@material-ui/core/colors/purple';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: deepPurple[500],
+    },
+    secondary: {
+      main: '#f44336',
+    },
+  },
+});
 
 const useStyles = makeStyles((theme) => ({
 
   root: {
-    maxWidth: 550,
+    maxWidth: 500,
+    minWidth: 450,
+    maxHeight: 560,
     padding: "100px 60px",
     margin: "120px auto",
     boxShadow: "0px 5px 10px #00000029;",
     border: "2px solid #EEEEEE",
     borderRadius: "9px",
     opacity: 1,
-    '& > * > button:hover': {
-      backgroundColor: "none",
-      opacity: "0.9",
-    },
+    action: {
+      margin: 0,
+    }
     // boxShadow: "",
   },
   btnGroup: {
@@ -48,6 +69,37 @@ const useStyles = makeStyles((theme) => ({
   btnProvider: {
     backgroundColor: "#998FA2",
   },
+  skip: {
+    maxWidth: 101,
+    float: "right",
+    minWidth: 100,
+    maxHeight: 36,
+    minHeight: 35,
+    '& > *': {
+      padding: "7px 15px",
+      margin: "auto",
+      fontSize: "16px",
+    },
+    // margin: "120px auto",
+    boxShadow: "0px 2px 5px #00000029;",
+    border: "1px solid #EEEEEE",
+    borderRadius: "5px",
+    opacity: 1,
+    margin: "10px",
+  },
+  rating: {
+    margin: "20px 0",
+    fontSize: "45px",
+  },
+  progBar: {
+    backgroundColor: "#F0E6FF",
+    height: "10px",
+    marginBottom: "15px",
+    borderRadius: "5px",
+    '& *': {
+      borderRadius: "5px",
+    },
+  },
   expand: {
     transform: 'rotate(0deg)',
     marginLeft: 'auto',
@@ -55,8 +107,22 @@ const useStyles = makeStyles((theme) => ({
       duration: theme.transitions.duration.shortest,
     }),
   },
-  expandOpen: {
-    transform: 'rotate(180deg)',
+  return: {
+    maxWidth: 101,
+    minWidth: 100,
+    maxHeight: 36,
+    minHeight: 35,
+    '& > *': {
+      padding: "5px",
+      margin: "auto",
+      fontSize: "16px",
+    },
+    // margin: "120px auto",
+    boxShadow: "0px 2px 5px #00000029;",
+    border: "1px solid #EEEEEE",
+    borderRadius: "5px",
+    opacity: 1,
+    margin: "10px",
   },
   avatar: {
     // backgroundColor: red[500],
@@ -69,41 +135,97 @@ const useStyles = makeStyles((theme) => ({
 export default function RecipeReviewCard() {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
+  const [value, setValue] = React.useState(2);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
   return (
-    <Grid container>
-      <AppBar position="static" color="inherit">
-        <Toolbar variant="dense">
-          <IconButton edge="start" aria-label="menu">
-            <ArrowBackIosIcon />SALIR
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Card className={classes.root}>
-        <CardHeader
-          avatar={
-            <img src={imgLog} className={classes.avatar} />
-          }
-          titleTypographyProps={{variant:'h5' }}
-          title="Company Name"
-          subheaderTypographyProps={{variant:'subtitle2' }}
-          subheader="Escoge la opción que más se ajuste al comentario que harás sobre esta empresa."
-        />
-        <CardContent>
-          <Typography variant="subtitle1" color="textSecondary" component="p">
-            Quiero opinar esta empresa como:
-          </Typography>
-          <Grid container direction="column" className={classes.btnGroup}>
-            <Button variant="contained" className={classes.btnClient}>CLIENTE</Button>
-            <Button variant="contained" className={classes.btnEmployee}>EMPLEADO</Button>
-            <Button variant="contained" className={classes.btnProvider}>PROVEEDOR</Button>
-          </Grid>
-        </CardContent>
-      </Card>
-    </Grid>
+
+    // ##########  Survey Start  ##############
+
+    // <Grid container>
+    //   <AppBar position="static" color="inherit">
+    //     <Toolbar variant="dense">
+    //       <IconButton edge="start" aria-label="menu">
+    //         <ArrowBackIosIcon />SALIR
+    //       </IconButton>
+    //     </Toolbar>
+    //   </AppBar>
+    //   <Card className={classes.root}>
+    //     <CardHeader
+    //       avatar={
+    //         <img src={imgLog} className={classes.avatar} />
+    //       }
+    //       titleTypographyProps={{variant:'h5' }}
+    //       title="Company Name"
+    //       subheaderTypographyProps={{variant:'subtitle2' }}
+    //       subheader="Escoge la opción que más se ajuste al comentario que harás sobre esta empresa."
+    //     />
+    //     <CardContent>
+    //       <Typography variant="subtitle1" color="textSecondary" component="p">
+    //         Quiero opinar esta empresa como:
+    //       </Typography>
+    //       <Grid container direction="column" className={classes.btnGroup}>
+    //         <Button variant="contained" color="primary" className={classes.btnClient}>CLIENTE</Button>
+    //         <Button variant="contained" color="secondary" className={classes.btnEmployee}>EMPLEADO</Button>
+    //         <Button variant="contained" color="inherit" className={classes.btnProvider}>PROVEEDOR</Button>
+    //       </Grid>
+    //     </CardContent>
+    //   </Card>
+    // </Grid>
+
+    // ##################################
+    <ThemeProvider theme={theme}>
+      <Grid container>
+        <AppBar position="static" color="inherit">
+          <Toolbar variant="dense">
+            <IconButton edge="start" aria-label="menu">
+              <ArrowBackIosIcon />SALIR
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Card className={classes.root}>
+          <CardHeader
+            avatar={
+              <img src={imgLog} className={classes.avatar} />
+            }
+            action={
+              <Card className={classes.return}>
+                <IconButton aria-label="menu">
+                  <SettingsBackupRestoreIcon />Regresar
+                </IconButton>
+              </Card>
+            }
+            titleTypographyProps={{variant:'h5' }}
+            title="Company Name"
+            subheaderTypographyProps={{variant:'h6' }}
+            subheader="Survey Name"
+          />
+          <CardContent>
+            <Typography variant="h6" color="textSecondary" component="p">
+              Question 1
+            </Typography>
+            <LinearProgress variant="buffer" value={33} className={classes.progBar} />
+            <Box component="fieldset" textAlign="center" mb={3} justifyContent="center" borderColor="transparent">
+              <Typography variant="h5" align="center" color="textSecondary" component="p">
+                Cómo te sientes con las políticas de calidad de la empresa?
+              </Typography>
+              <Rating
+                name="simple-controlled"
+                value={5}
+                className={classes.rating}
+              />
+            </Box>
+            <Card className={classes.skip}>
+              <IconButton aria-label="skip">
+                Omitir<ArrowForwardIcon />
+              </IconButton>
+            </Card>
+          </CardContent>
+        </Card>
+      </Grid>
+    </ThemeProvider>
   );
 }
--- src/views/dist/SruveyStart.js
+++ src/views/dist/SruveyStart.js
@@ -8,24 +8,42 @@ var Typography_1 = require("@material-ui/core/Typography");
 var IconButton_1 = require("@material-ui/core/IconButton");
 var Card_1 = require("@material-ui/core/Card");
 var Grid_1 = require("@material-ui/core/Grid");
-var Button_1 = require("@material-ui/core/Button");
 var CardHeader_1 = require("@material-ui/core/CardHeader");
 var CardContent_1 = require("@material-ui/core/CardContent");
 var ArrowBackIos_1 = require("@material-ui/icons/ArrowBackIos");
+var ArrowForward_1 = require("@material-ui/icons/ArrowForward");
+var SettingsBackupRestore_1 = require("@material-ui/icons/SettingsBackupRestore");
+var LinearProgress_1 = require("@material-ui/core/LinearProgress");
+var Box_1 = require("@material-ui/core/Box");
+var Rating_1 = require("@material-ui/lab/Rating");
 var car_png_1 = require("../assets/img/car.png");
+var styles_2 = require("@material-ui/core/styles");
+var purple_1 = require("@material-ui/core/colors/purple");
+var theme = styles_2.createMuiTheme({
+    palette: {
+        primary: {
+            main: purple_1["default"][500]
+        },
+        secondary: {
+            main: '#f44336'
+        }
+    }
+});
 var useStyles = styles_1.makeStyles(function (theme) { return ({
     root: {
-        maxWidth: 550,
+        maxWidth: 500,
+        minWidth: 450,
+        maxHeight: 560,
         padding: "100px 60px",
         margin: "120px auto",
         boxShadow: "0px 5px 10px #00000029;",
         border: "2px solid #EEEEEE",
         borderRadius: "9px",
         opacity: 1,
-        '& > * > button:hover': {
-            backgroundColor: "none",
-            opacity: "0.9"
+        action: {
+            margin: 0
         }
+        // boxShadow: "",
     },
     btnGroup: {
         padding: '10px 0',
@@ -46,6 +64,37 @@ var useStyles = styles_1.makeStyles(function (theme) { return ({
     btnProvider: {
         backgroundColor: "#998FA2"
     },
+    skip: {
+        maxWidth: 101,
+        float: "right",
+        minWidth: 100,
+        maxHeight: 36,
+        minHeight: 35,
+        '& > *': {
+            padding: "7px 15px",
+            margin: "auto",
+            fontSize: "16px"
+        },
+        // margin: "120px auto",
+        boxShadow: "0px 2px 5px #00000029;",
+        border: "1px solid #EEEEEE",
+        borderRadius: "5px",
+        opacity: 1,
+        margin: "10px"
+    },
+    rating: {
+        margin: "20px 0",
+        fontSize: "45px"
+    },
+    progBar: {
+        backgroundColor: "#F0E6FF",
+        height: "10px",
+        marginBottom: "15px",
+        borderRadius: "5px",
+        '& *': {
+            borderRadius: "5px"
+        }
+    },
     expand: {
         transform: 'rotate(0deg)',
         marginLeft: 'auto',
@@ -53,8 +102,22 @@ var useStyles = styles_1.makeStyles(function (theme) { return ({
             duration: theme.transitions.duration.shortest
         })
     },
-    expandOpen: {
-        transform: 'rotate(180deg)'
+    "return": {
+        maxWidth: 101,
+        minWidth: 100,
+        maxHeight: 36,
+        minHeight: 35,
+        '& > *': {
+            padding: "5px",
+            margin: "auto",
+            fontSize: "16px"
+        },
+        // margin: "120px auto",
+        boxShadow: "0px 2px 5px #00000029;",
+        border: "1px solid #EEEEEE",
+        borderRadius: "5px",
+        opacity: 1,
+        margin: "10px"
     },
     avatar: {
         // backgroundColor: red[500],
@@ -66,22 +129,64 @@ var useStyles = styles_1.makeStyles(function (theme) { return ({
 function RecipeReviewCard() {
     var classes = useStyles();
     var _a = react_1["default"].useState(false), expanded = _a[0], setExpanded = _a[1];
+    var _b = react_1["default"].useState(2), value = _b[0], setValue = _b[1];
     var handleExpandClick = function () {
         setExpanded(!expanded);
     };
-    return (react_1["default"].createElement(Grid_1["default"], { container: true },
-        react_1["default"].createElement(AppBar_1["default"], { position: "static", color: "inherit" },
-            react_1["default"].createElement(Toolbar_1["default"], { variant: "dense" },
-                react_1["default"].createElement(IconButton_1["default"], { edge: "start", "aria-label": "menu" },
-                    react_1["default"].createElement(ArrowBackIos_1["default"], null),
-                    "SALIR"))),
-        react_1["default"].createElement(Card_1["default"], { className: classes.root },
-            react_1["default"].createElement(CardHeader_1["default"], { avatar: react_1["default"].createElement("img", { src: car_png_1["default"], className: classes.avatar }), titleTypographyProps: { variant: 'h5' }, title: "Company Name", subheaderTypographyProps: { variant: 'subtitle2' }, subheader: "Escoge la opci\u00F3n que m\u00E1s se ajuste al comentario que har\u00E1s sobre esta empresa." }),
-            react_1["default"].createElement(CardContent_1["default"], null,
-                react_1["default"].createElement(Typography_1["default"], { variant: "subtitle1", color: "textSecondary", component: "p" }, "Quiero opinar esta empresa como:"),
-                react_1["default"].createElement(Grid_1["default"], { container: true, direction: "column", className: classes.btnGroup },
-                    react_1["default"].createElement(Button_1["default"], { variant: "contained", className: classes.btnClient }, "CLIENTE"),
-                    react_1["default"].createElement(Button_1["default"], { variant: "contained", className: classes.btnEmployee }, "EMPLEADO"),
-                    react_1["default"].createElement(Button_1["default"], { variant: "contained", className: classes.btnProvider }, "PROVEEDOR"))))));
+    return (
+    // ##########  Survey Start  ##############
+    // <Grid container>
+    //   <AppBar position="static" color="inherit">
+    //     <Toolbar variant="dense">
+    //       <IconButton edge="start" aria-label="menu">
+    //         <ArrowBackIosIcon />SALIR
+    //       </IconButton>
+    //     </Toolbar>
+    //   </AppBar>
+    //   <Card className={classes.root}>
+    //     <CardHeader
+    //       avatar={
+    //         <img src={imgLog} className={classes.avatar} />
+    //       }
+    //       titleTypographyProps={{variant:'h5' }}
+    //       title="Company Name"
+    //       subheaderTypographyProps={{variant:'subtitle2' }}
+    //       subheader="Escoge la opción que más se ajuste al comentario que harás sobre esta empresa."
+    //     />
+    //     <CardContent>
+    //       <Typography variant="subtitle1" color="textSecondary" component="p">
+    //         Quiero opinar esta empresa como:
+    //       </Typography>
+    //       <Grid container direction="column" className={classes.btnGroup}>
+    //         <Button variant="contained" color="primary" className={classes.btnClient}>CLIENTE</Button>
+    //         <Button variant="contained" color="secondary" className={classes.btnEmployee}>EMPLEADO</Button>
+    //         <Button variant="contained" color="inherit" className={classes.btnProvider}>PROVEEDOR</Button>
+    //       </Grid>
+    //     </CardContent>
+    //   </Card>
+    // </Grid>
+    // ##################################
+    react_1["default"].createElement(styles_1.ThemeProvider, { theme: theme },
+        react_1["default"].createElement(Grid_1["default"], { container: true },
+            react_1["default"].createElement(AppBar_1["default"], { position: "static", color: "inherit" },
+                react_1["default"].createElement(Toolbar_1["default"], { variant: "dense" },
+                    react_1["default"].createElement(IconButton_1["default"], { edge: "start", "aria-label": "menu" },
+                        react_1["default"].createElement(ArrowBackIos_1["default"], null),
+                        "SALIR"))),
+            react_1["default"].createElement(Card_1["default"], { className: classes.root },
+                react_1["default"].createElement(CardHeader_1["default"], { avatar: react_1["default"].createElement("img", { src: car_png_1["default"], className: classes.avatar }), action: react_1["default"].createElement(Card_1["default"], { className: classes["return"] },
+                        react_1["default"].createElement(IconButton_1["default"], { "aria-label": "menu" },
+                            react_1["default"].createElement(SettingsBackupRestore_1["default"], null),
+                            "Regresar")), titleTypographyProps: { variant: 'h5' }, title: "Company Name", subheaderTypographyProps: { variant: 'h6' }, subheader: "Survey Name" }),
+                react_1["default"].createElement(CardContent_1["default"], null,
+                    react_1["default"].createElement(Typography_1["default"], { variant: "h6", color: "textSecondary", component: "p" }, "Question 1"),
+                    react_1["default"].createElement(LinearProgress_1["default"], { variant: "buffer", value: 33, className: classes.progBar }),
+                    react_1["default"].createElement(Box_1["default"], { component: "fieldset", textAlign: "center", mb: 3, justifyContent: "center", borderColor: "transparent" },
+                        react_1["default"].createElement(Typography_1["default"], { variant: "h5", align: "center", color: "textSecondary", component: "p" }, "C\u00F3mo te sientes con las pol\u00EDticas de calidad de la empresa?"),
+                        react_1["default"].createElement(Rating_1["default"], { name: "simple-controlled", value: 5, className: classes.rating })),
+                    react_1["default"].createElement(Card_1["default"], { className: classes.skip },
+                        react_1["default"].createElement(IconButton_1["default"], { "aria-label": "skip" },
+                            "Omitir",
+                            react_1["default"].createElement(ArrowForward_1["default"], null))))))));
 }
 exports["default"] = RecipeReviewCard;
