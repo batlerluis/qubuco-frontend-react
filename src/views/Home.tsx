5408491d70a4b942eba2b7a3b4ed6df902a94336
unknown <batlerluis@yandex.com>
2020-09-07 03:05:46 +0300

    AutoComplete

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -96,12 +96,12 @@ const pageStyle = {
 };
 
 const top100Films = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
+  { title: 'The Shawshank Redemption', year: 1994, year1: 'Belgium' },
+  { title: 'The Godfather', year: 1972, year1: 'Australlia' },
+  { title: 'The Godfather: Part II', year: 1974, year1: 'USA' },
+  { title: 'The Dark Knight', year: 2008, year1: 'China' },
+  { title: '12 Angry Men', year: 1957, year1: 'France' },
+  { title: "Schindler's List", year: 1993, year1: 'Brazil' },
 ];
 
 // const imgPhone = "http://localhost:3000/phone.png";
@@ -156,7 +156,7 @@ const Home = (props: any) => {
                         filtered.push({
                           inputValue: params.inputValue,
                           title: `${params.inputValue} `,
-                          year: "Add",
+                          year: "Sé el primero en comentar sobre esta empresa",
                         });
                       }
                       return filtered;
@@ -171,6 +171,9 @@ const Home = (props: any) => {
                             </span>
                             <Typography variant="body2" color="textSecondary">
                               {option.year}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary">
+                              {option.year1}
                             </Typography>
                           </Grid>
                         </Grid>
